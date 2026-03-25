a4db0c3137e2ecd1219e25d8e88f2404059cf304
Syping <syping@syping.de>
2026-03-25 23:59:40 +0100

    [skip ci] message_queue: use const for direct message target string

--- src/core/message_queue.cpp
+++ src/core/message_queue.cpp
@@ -108,7 +108,7 @@ void message_queue::run(bot::settings::settings *settings, submit_queue *submit_
             if (const auto *direct_message = std::get_if<bot::direct_message>(&message)) {
                 const std::lock_guard<bot::settings::settings> guard(*settings);
                 bot::settings::user *user = settings->get_user(direct_message->event.command.get_issuing_user().id);
-                std::string target = user ? user->target : "en";
+                const std::string target = user ? user->target : "en";
                 translated_direct_message translated_message;
                 translated_message.event = direct_message->event;
                 translated_message.message = translator->translate(direct_message->message, {}, target);
